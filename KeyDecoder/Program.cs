955326ae3cf329b4c8e7478f6db0e1d8eddf7396
SANGWOO PARK <cyberprophet@hanmail.net>
2023-09-07 13:03:47 +0900

    초기 커밋.

---
+++ KeyDecoder/Program.cs
@@ -0,0 +1,11 @@
+﻿using ShareInvest.Identifies;
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("windows8.0")]
+
+if (KeyDecoder.ProductKeyFromRegistry is string key)
+{
+    Console.WriteLine(key);
+
+    Console.ReadLine();
+}
